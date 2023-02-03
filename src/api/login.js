85a5995000f953580785ca8e144d49ebfcb38cc2
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2023-02-03 22:50:44 +0300

    During login, the required user information has been added to the login folder under API.

---
+++ src/api/login.js
@@ -0,0 +1,11 @@
+const people = [
+  {
+    id: 1,
+    email: "aslantas-mehmet58@hotmail.com",
+    password: "12345",
+    name: "Mehmet ASLANTAŞ",
+    title: "Bina Sakini",
+  },
+];
+
+export default people;
